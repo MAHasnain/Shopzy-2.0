3c843617b8be2a3e5492c542ab3f80e0727941f1
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-19 02:12:15 +0500

    feat: Completed database methods for orders and products in Shopzy project

---
+++ Database/orders.database.js
@@ -0,0 +1,32 @@
+const SUPABASE_URL = `https://ufeiacuottjvkuezhyue.supabase.co`;
+const SUPABASE_ANON_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZWlhY3VvdHRqdmt1ZXpoeXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MTQ3MDgsImV4cCI6MjA3NjE5MDcwOH0.3nRXCTj84tAReLv6w9c0QIDCzuYphEo64F3tQn0BQ30`;
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const orderCreation = async (orderData) => {
+    const { data, error } = await supabaseClient
+        .from('Orders')
+        .insert(orderData)
+        .select()
+
+    if (error) {
+        console.error(error);
+        return error;
+    }
+    console.log(data);
+    return data;
+}
+
+const getOrders = async () => {
+    const { data, error } = await supabaseClient
+        .from('Orders')
+        .select("*")
+
+    if (error) {
+        console.error(error);
+        return error;
+    }
+    console.log(data);
+    return data;
+
+}
---
+++ Database/products.database.js
@@ -0,0 +1,38 @@
+const SUPABASE_URL = `https://ufeiacuottjvkuezhyue.supabase.co`;
+const SUPABASE_ANON_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZWlhY3VvdHRqdmt1ZXpoeXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MTQ3MDgsImV4cCI6MjA3NjE5MDcwOH0.3nRXCTj84tAReLv6w9c0QIDCzuYphEo64F3tQn0BQ30`;
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// console.log(supabase);
+// console.log(supabaseClient);
+
+const getAllProducts = async () => {
+
+    const { data, error } = await supabaseClient
+        .from('Products')
+        .select("*")
+
+    if (error) {
+        console.log(error);
+        return error;
+    }
+
+    console.log(data);
+    return data;
+
+}
+
+const getProductById = async (id) => {
+     const { data, error } = await supabaseClient
+        .from("Products")
+        .select("*")
+        .eq('id', id)
+        .single();
+
+    if (error) {
+        console.log(error);
+        return error;
+    }
+    // console.log(data);
+    return data;
+}
