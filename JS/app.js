1bf7af720ad05b00bd66ca4542ede3c1e676bb16
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-28 19:18:38 +0500

    feat: added home page layout and logic to render all products and categories in Shopzy 2.0 project

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,34 @@
-// import { getAllProducts } from "../Database/products.database.js";
-
-
 // console.log(getAllProducts);
-console.log(supabase);
+// console.log(supabase);
+
+import { getAllCategories, getAllProducts } from "../Database/allMethods.js";
+
+const productsContainer = document.querySelector(".products_container")
+document.addEventListener("DOMContentLoaded", async () => {
+    const allProducts = await getAllProducts();
+    console.log(allProducts);
+
+    allProducts.map(product => {
+        productsContainer.innerHTML += `
+         <div class="product-card" data-id="${product.id}">
+                <img src="${product.image_url}" width=200px alt="">
+                    <div class="product-price"><h4>Rs. ${product.price}</h4></div>
+                    <div class="product-name"><p>${product.title}</p></div>
+                    <div class="product-description"><p>${product.description}</p></div>
+                    <div class="product-stock"><p>${product.stock}</p></div>
+                    
+                    <button id="cart_btn">Add to Cart</button>
+                </div>`
+    })
+
+    const productCards = document.querySelectorAll(".product-card");
+    productCards.forEach(card => {
+        card.addEventListener("click", () => {
+            const cardId = card.getAttribute("data-id")
+            window.location.href = `../HTML/product_details.html?id=${cardId}`
+        })
+    })
+
+    const allCategories = await getAllCategories();
+    console.log(allCategories);
+})
