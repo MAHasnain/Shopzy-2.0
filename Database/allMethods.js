5de701aa8f574b2eea2f4175016420a6a0b3a336
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-29 07:59:00 +0500

    refactor: optimized imports and exports by consolidating all database methods into a single file

---
+++ Database/allMethods.js
@@ -0,0 +1,28 @@
+import { createNewUser, getLoggedInUser, getUserSession, signInUser, signOutUser } from "./auth.database.js";
+import { addProductInCart, editProductInCart, removeMultipleProducts, removeProductById } from "./cart.database.js";
+import { getAllCategories, getCategoryById } from "./categories.database.js";
+import { getAllProducts, getProductById } from "./products.database.js";
+import { getOrders, orderCreation } from "./orders.database.js";
+
+export {
+    // Auth Methods
+    createNewUser,  
+    signInUser,
+    signOutUser,
+    getUserSession,
+    getLoggedInUser,
+    // Cart Methods
+    addProductInCart,
+    editProductInCart,
+    removeProductById,
+    removeMultipleProducts,
+    // Categories Methods
+    getAllCategories,
+    getCategoryById,
+    // Products Methods
+    getAllProducts, 
+    getProductById,
+    // Orders Methods
+    getOrders,
+    orderCreation
+};
