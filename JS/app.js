0820b430ddbf59f74f9e2e7b128eb05629e194ce
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-11-15 19:57:27 +0500

    - Added header with navigation and brand logo - Implemented clean footer section with quick links & copyright text - Updated global styles for consistent spacing and typography - Integrated header & footer across pages - Home page layout almost completed with finalized structure and styling

--- JS/app.js
+++ JS/app.js
@@ -5,21 +5,32 @@ import { getAllCategories, getAllProducts } from "../Database/allMethods.js";
 
 const productsContainer = document.querySelector(".products_container")
 const categoriesContainer = document.querySelector(".categories")
+
+function shortText(text, limit = 30) {
+    return text.length > limit ? text.slice(0, limit) + "..." : text;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const allProducts = await getAllProducts();
     console.log(allProducts);
 
     allProducts.map(product => {
         productsContainer.innerHTML += `
-         <div class="product-card" data-id="${product.id}">
-                <img src="${product.image_url}" width=200px alt="">
-                    <div class="product-price"><h4>Rs. ${product.price}</h4></div>
-                    <div class="product-name"><p>${product.title}</p></div>
-                    <div class="product-description"><p>${product.description}</p></div>
-                    <div class="product-stock"><p>${product.stock}</p></div>
-                    
-                    <button id="cart_btn">Add to Cart</button>
-                </div>`
+        <div class="product-card" data-id="${product.id}">
+        <div>
+        <img src="${product.image_url}" width=200px alt=""></div>
+        <div class="product-price"><h4>Rs. ${product.price}</h4></div>
+        <div class="product-name"><p>${product.title}</p></div>
+        <div class="product-description"><p>${product.description}</p></div>
+        <div class="product-stock"><p>${product.stock}</p></div>
+        
+        <div><button class="btn" id="cart_btn">Add to Cart</button></div>
+        </div>`
+
+        const descEl = document.querySelectorAll(".product-description");
+        descEl.forEach(el => {
+            el.textContent = shortText(el.textContent, 50);
+        });
     })
 
     const productCards = document.querySelectorAll(".product-card");
@@ -33,7 +44,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const allCategories = await getAllCategories();
     console.log(allCategories);
     allCategories.map(category => {
-        categoriesContainer.innerHTML += `                <div data-id="${category.id}" class="category-card">
+        categoriesContainer.innerHTML += `<div data-id="${category.id}" class="category-card card">
             <p>${category.name}</p>
         </div>
         `
@@ -43,7 +54,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     categoryCards.forEach(card => {
         card.addEventListener("click", () => {
             const cardId = card.getAttribute("data-id");
-             window.location.href = `../HTML/category_details.html?id=${cardId}`
+            window.location.href = `../HTML/category_details.html?id=${cardId}`
         })
     })
 
